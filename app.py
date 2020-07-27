68b1943998ebc65b4ac0c707767b2dfb56326dc0
Шарафудинов Ануар (ext) <anuar.sharafutdinov@gmail.com>
2020-07-27 23:52:30 +0600

    2nd

--- app.py
+++ app.py
@@ -15,9 +15,15 @@ from flask import request
 from flask import jsonify
 
 from sklearn.externals import joblib
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_squared_error, average_precision_score
 from sklearn.utils import shuffle    
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+#from skmultilearn.problem_transform import BinaryRelevance
+#from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+#from sklearn.linear_model import LogisticRegression
+#from skmultilearn.problem_transform import BinaryRelevance
+
 
 PRODUCT_CATEGORIES_N = 49
 USER_CATEGORIES_N = 23
@@ -76,50 +82,89 @@ def refill_prepare():
 	print(stations)
 
 
-	X = []
+	Xd = []
+	Xs = []
 	Y_date_diff = []
 	Y_station_idx = []
 	for login in d:
-		print(d[login])
-		print("\n")
+		#print(d[login])		
 		a = d[login]
-		for i in range(0, len(a)-3, 3):
+		for i in range(0, len(a)-3, 2):
 			#x = [ [a[i]['date_diff'], a[i]['station_idx']], [a[i+1]['date_diff'], a[i+1]['station_idx']], [a[i+2]['date_diff'], a[i+2]['station_idx']] ]
-			x = [ a[i]['date_diff'], a[i]['station_idx'], a[i+1]['date_diff'], a[i+1]['station_idx'], a[i+2]['date_diff'], a[i+2]['station_idx'] ]
-			y_date_diff = a[i+3]['date_diff']
-			y_station_idx = a[i+3]['station_idx']
-			X.append(x)
+			dd0 = a[i+0]['date_diff'] - a[i]['date_diff']
+			dd1 = a[i+1]['date_diff'] - a[i]['date_diff']
+			dd2 = a[i+2]['date_diff'] - a[i]['date_diff']
+			dd3 = a[i+3]['date_diff'] - a[i]['date_diff']
+			s0 = a[i]['station_idx']
+			s1 = a[i+1]['station_idx']
+			s2 = a[i+2]['station_idx']
+			s3 = a[i+3]['station_idx']
+			xd = [dd0, s0, dd1, s1, dd2, s2]
+			#xs = [ dd0, s0, dd1, s1, dd2, s2 ] #[s0, s1, s2] #
+			xs = [0] * len(stations)
+			xs[s0] += 1
+			xs[s1] += 1
+			xs[s2] += 1
+			y_date_diff = dd3
+			y_station_idx = s3
+			Xd.append(xd)
+			Xs.append(xs)
 			Y_date_diff.append(y_date_diff)
 			Y_station_idx.append(y_station_idx)
 
-	file_put_contents("refill_X.json", json.dumps(X))
+	file_put_contents("refill_Xd.json", json.dumps(Xd))
+	file_put_contents("refill_Xs.json", json.dumps(Xs))
 	file_put_contents("refill_Y_date_diff.json", json.dumps(Y_date_diff))
 	file_put_contents("refill_Y_station_idx.json", json.dumps(Y_station_idx))        
-#asda
+	file_put_contents("stations.json", json.dumps(stations))
+
+
 
 def refill_train():
 	DIVIDER = 150
-
-
-	with open('refill_X.json') as f: X = json.load(f) #1823
+	with open('refill_Xd.json') as f: Xd = json.load(f)
+	with open('refill_Xs.json') as f: Xs = json.load(f)
 	with open('refill_Y_date_diff.json') as f: Yd = json.load(f)  
-	with open('refill_Y_station_idx.json') as f: Ys = json.load(f)  
-	
+	with open('refill_Y_station_idx.json') as f: Ys = json.load(f)  	
 
 	clf_d = RandomForestRegressor(n_estimators=100) 
-	clf_s = RandomForestClassifier(n_estimators=100) 
-	clf_d.fit(X[DIVIDER:],Yd[DIVIDER:])
-	clf_s.fit(X[DIVIDER:],Ys[DIVIDER:])
+	#clf_s = RandomForestClassifier(n_estimators=100) 
+	clf_s = svm.LinearSVC()
+	clf_d.fit(Xd[DIVIDER:],Yd[DIVIDER:])
+	clf_s.fit(Xs[DIVIDER:],Ys[DIVIDER:])
 	joblib.dump(clf_d, 'model_refill_d.save')    
 	joblib.dump(clf_s, 'model_refill_s.save')    
 
-	predictions_d = clf_d.predict(X[:DIVIDER])
-	predictions_s = clf_s.predict(X[:DIVIDER])
+	predictions_d = clf_d.predict(Xd[:DIVIDER])
+	predictions_s = clf_s.predict(Xs[:DIVIDER])
 	for i in range(len(predictions_s)):
-		print(predictions_d[i], Yd[i])
+		print(int(predictions_d[i]), Yd[i])
 	
+	print(mean_squared_error(Yd[:DIVIDER], predictions_d))
 	print(accuracy_score(Ys[:DIVIDER], predictions_s))
 
+
+
+def refill_predict(arr): #[{'date_diff':0, 'login':'', 'station':''}, {}]
+	with open('stations.json') as f: stations = json.load(f)
+	dd0 = arr[0]['date_diff']
+	s0 = stations.index(arr[0]['station'])
+	dd1 = arr[1]['date_diff']
+	s1 = stations.index(arr[1]['station'])
+	dd2 = arr[2]['date_diff']
+	s2 = stations.index(arr[2]['station'])
+	xd = [ dd0, s0, dd1, s1, dd2, s2 ]
+	#xs = [ dd0, s0, dd1, s1, dd2, s2 ] #
+	xs = [0] * len(stations)
+	xs[s0] += 1
+	xs[s1] += 1
+	xs[s2] += 1
+
+	clf_d = joblib.load('model_refill_d.save') 	
+	clf_s = joblib.load('model_refill_s.save') 	
+	pre_d = int(clf_d.predict([xd])[0])
+	pre_s = clf_s.predict([xs])[0]
+	return pre_d, stations[pre_s]
 #endOf fuel
 
 
@@ -170,6 +215,7 @@ def get_products_map():
 	products = cursor.fetchall()                
 	for p in products:
 		products_map[p["particle"]] = p["pcategory"]
+	file_put_contents("products_map.json", json.dumps(products_map))
 	return products_map
 
 
@@ -201,13 +247,6 @@ def prepare_dataset():
 
 def train():
 	DIVIDER = 150
-
-	#from skmultilearn.problem_transform import BinaryRelevance
-	#from sklearn.naive_bayes import GaussianNB
-	#from sklearn import svm
-	#from sklearn.linear_model import LogisticRegression
-	#from skmultilearn.problem_transform import BinaryRelevance
-
 	with open('X.json') as f: Xin = json.load(f)
 	with open('Y.json') as f: Yin = json.load(f)  
 	with open('infos.json') as f: infos = json.load(f)  
@@ -216,15 +255,13 @@ def train():
 
 	input_len = PRODUCT_CATEGORIES_N +1 #max(Xin)[0] + 1
 	classes_n = USER_CATEGORIES_N +1 #max(Yin)[0] + 1
-	print(input_len, classes_n, len(Xin))
-	
+	print(input_len, classes_n, len(Xin))	
 	X = []
 	Y = []
 	for row in Xin:
 		a = [0]*input_len
 		for x in row: a[x] = 1
 		X.append(a)
-
 	
 	for row in Yin:
 		a = [0] * classes_n        
@@ -286,12 +323,14 @@ def train():
 		print(accuracy_score(Y[:DIVIDER], predictions))
 
 
+
 def predict(particles): #[2,4,5]
-	products_map = get_products_map() #particle -> pcategory
+	#products_map = get_products_map() #particle -> pcategory
+	with open('products_map.json') as f: products_map = json.load(f)	
 	clf = joblib.load('model.save') 	
 	input_len = PRODUCT_CATEGORIES_N + 1
 	x = [0] * input_len
-	for particle in particles: x[products_map[particle]] = 1
+	for particle in particles: x[products_map[str(particle)]] = 1
 	pre = clf.predict([x])[0]
 	user_categories = []
 	for i in range(len(pre)):
@@ -312,29 +351,27 @@ def home():
 		particles = json.loads(request.args.get('particles'))
 		print("uc_predict", particles)
 		return jsonify(predict(particles))
-
 		
-	elif action=="w2l_decode": 
-		url= request.args.get('url')
-		text = request.args.get('text')
-		subprocess.check_output(['wget', '-O', '/home/ubuntu/shokhan/w2l_kazru/data/repoint/temp/temp.wav', url])
-		st = 'repoint_test1  /home/ubuntu/shokhan/w2l_kazru/data/repoint/temp/temp.wav 1.0 '+str(text)
-		file_put_contents("/home/ubuntu/shokhan/w2l_kazru/data/repoint/test_repoint.lst", st)
-		res = subprocess.check_output(["sudo", "./test.sh"])        
-		res = file_get_contents("/home/ubuntu/shokhan/w2l_kazru/archdir/output.txt")
-		print(res)
-		return str(res)
+	elif action=="refill_predict": 
+		#http://zapravka.ailabs.kz:5000?action=refill_predict&arr=[{"station": "GasOilProm 4 BJ", "date_diff": 0, "login": 0}, {"station": "GasOilProm 4 BJ", "date_diff": 5, "login": 0}, {"station": "GasOilProm 4 BJ", "date_diff": 25, "login": 0}]
+		arr = json.loads(request.args.get('arr'))
+		return jsonify(refill_predict(arr))
 	return "no action"
 
 	
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port='5000')
+	
 	#recommendations system
 	#import_checks(2)
 	#prepare_dataset() #1
 	#train() #2
 	#print(predict([2025, 33101, 33103])) #8,9
 
-	#fuel
+
+	#refill
 	#refill_prepare() #1
 	#refill_train() #2
+	#arr = [{'date_diff':0, 'station':'GasOilProm 4 BJ', 'login':0}, {'date_diff':5, 'station':'GasOilProm 4 BJ', 'login':0}, {'date_diff':25, 'station':'GasOilProm 4 BJ', 'login':0}]
+	#print(refill_predict(arr))
+
